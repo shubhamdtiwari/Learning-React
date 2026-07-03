1964700617283e02a585daac604d78c8d8ea772d
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-07-03 20:07:33 +0530

    Update App.jsx

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -1,15 +1,12 @@
 import { useState } from "react";
 
 import "./App.css";
+import Home from "./components/Home/Home";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <h1 className="bg-green-600 p-4">React router</h1>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
