8f5be0450e3250f40742a7615e8997d476275608
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-07-05 23:53:30 +0530

    Update main.jsx

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -1,12 +1,28 @@
 import { StrictMode } from "react";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./Layout.jsx";
+import Home from "./components/Home/Home.jsx";
+import About from "./components/About/About.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
   },
 ]);
 
