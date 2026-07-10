13b59c81ed6efceb677faa1620d8fc66bb7a05bd
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-07-10 20:19:58 +0530

    Update User.jsx

--- 07reactRouter/src/components/User/User.jsx
+++ 07reactRouter/src/components/User/User.jsx
@@ -1,7 +1,10 @@
 import React from "react";
-
+import { useParams } from "react-router-dom";
 function User() {
-  return <div>User: {userid}</div>;
+  const { userid } = useParams();
+  return (
+    <div className="bg-gray-600 text-white text-3xl p-4">User: {userid}</div>
+  );
 }
 
 export default User;
