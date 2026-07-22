b43e9ca2cbef67e84b4edac88bd2487b23384948
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-07-22 23:57:40 +0530

    Update UserContextProvider.jsx

--- 08minicontext/src/context/UserContextProvider.jsx
+++ 08minicontext/src/context/UserContextProvider.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import UserContext from "./UserContext";
 
-const UserContextProvider = ({}) => {};
+const UserContextProvider = ({children}) => {
+    const [user, setUser] = React.useState(null)
+    return(
+        <UserContext.Provider>
+        {children}
+        <UserContext.Provider/>
+    )
+};
 
 export default UserContextProvider;
