a148e47d0404dd93da02a7a6e15f145dd7ffe5ee
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-07-23 00:09:44 +0530

    Update App.jsx

--- 08minicontext/src/App.jsx
+++ 08minicontext/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import "./App.css";
+import UserContextProvider from "./context/UserContextProvider";
 
 function App() {
   return (
-    <>
+    <UserContextProvider>
       <h1>React with Chai</h1>
-    </>
+    </UserContextProvider>
   );
 }
 
