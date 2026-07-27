e3ea00f753a2dbe4c91ace42aab20432dcfebeee
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-07-27 23:41:00 +0530

    Update Login.jsx

--- 08minicontext/src/components/Login.jsx
+++ 08minicontext/src/components/Login.jsx
@@ -11,6 +11,24 @@ function Login() {
     e.preventDefault();
     setUser({ username, password });
   };
-  
+  return (
+    <div>
+      <h2>Login</h2>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="username"
+      />
+      <input
+        type="text"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="password"
+      />
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
+}
 
 export default Login;
